02a27b08d7c3eed38bcbee7d5a6bcc8b14272bd7
jmpascagaza53 <mrphoenix192@gmail.com>
2017-03-27 18:58:18 -0500

    Add files via upload

---
+++ JerseyClientGet.java
@@ -0,0 +1,15 @@
+package consumir;
+import consumir.servico;
+public class JerseyClientGet {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		servico c =new servico();
+		
+		
+	}
+
+}
